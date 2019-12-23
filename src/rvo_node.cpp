48355d7b790f52eb174a9d04345015cecaeea3e2
hanruihua <744475118@qq.com>
2019-12-23 09:44:40 +0800

    modify the topics

--- src/rvo_node.cpp
+++ src/rvo_node.cpp
@@ -1,13 +1,13 @@
 #include "rvo_node.h"
 
 int cout_flag = 0;
-
+int seq = 0;
 int main(int argc, char **argv)
 {
 
     ros::init(argc, argv, "rvo_node");
     ros::NodeHandle n;
-    rvo_node_pub = n.advertise<gazebo_msgs::ModelStates>("rvo_vel", 1000);
+    rvo_node_pub = n.advertise<gazebo_msgs::WorldState>("/rvo_vel", 1000);
     ros::Subscriber sub = n.subscribe("/rvo/model_states", 100, rvo_velCallback);
     ros::ServiceServer service = n.advertiseService("set_rvo_goals", set_goals);
     ros::Rate loop_rate(50);
@@ -30,9 +30,6 @@ int main(int argc, char **argv)
     while (ros::ok())
     {
         ros::spinOnce();
-        cout_flag++;
-        if (cout_flag > 2500)
-            std::cout<<"waiting for message of model_states"<<std::endl;
         loop_rate.sleep();
     }
 }
@@ -100,8 +97,9 @@ void rvo_goals_init()
 
 void rvo_velCallback(const gazebo_msgs::ModelStates::ConstPtr &sub_msg)
 {
-    // std::cout<<num_agent<<std::endl;
-    cout_flag = 0;
+
+    seq++;
+    int count_vel = 0;
 
     rvo->updateState_gazebo(sub_msg); // read the message
     if (motion_model == "default")
@@ -117,9 +115,16 @@ void rvo_velCallback(const gazebo_msgs::ModelStates::ConstPtr &sub_msg)
     auto models_name = sub_msg->name;
     int total_num = models_name.size();
 
+    std_msgs::Header header;
+    header.frame_id = "global";
+    header.stamp = ros::Time::now();
+    header.seq = seq;
+
     msg_pub.name.clear();
-    msg_pub.twist.clear();
     msg_pub.pose.clear();
+    msg_pub.twist.clear();
+    
+    msg_pub.header = header;
 
     num_agent = new_velocities.size();
 
@@ -129,43 +134,36 @@ void rvo_velCallback(const gazebo_msgs::ModelStates::ConstPtr &sub_msg)
         copy_num_agent = num_agent;
     }
 
-    int count = 0;
-
     for (int i = 0; i < total_num; i++)
     {
         geometry_msgs::Twist new_vel;
-        geometry_msgs::Pose rvo_pose;
+        geometry_msgs::Pose cur_pose;
+
         std::string agent_name = "agent" + std::to_string(i + 1);
 
         auto iter_agent = std::find(models_name.begin(), models_name.end(), agent_name);
-        int agent_index = iter_agent - models_name.begin();
+        int iter_index = iter_agent - models_name.begin();
 
+        
         if (iter_agent != models_name.end())
         {
-            rvo_pose = sub_msg->pose[agent_index];
-
-            float x = new_velocities[count]->x();
-            float y = new_velocities[count]->y();
-
-            // float speed = sqrt(x * x + y * y);
-
-            // float ratio = vel_ratio(speed, 0.2f, 0.3f);
+            
+            float x = new_velocities[count_vel]->x();
+            float y = new_velocities[count_vel]->y();
 
             new_vel.linear.x = x;
             new_vel.linear.y = y;
 
+            cur_pose = sub_msg->pose[iter_index];
+
             msg_pub.name.push_back(agent_name);
             msg_pub.twist.push_back(new_vel);
-            msg_pub.pose.push_back(rvo_pose);
+            msg_pub.pose.push_back(cur_pose);
 
-            count++;
+            count_vel++;
             std::cout << "Current " << agent_name << std::endl;
         }
     }
     rvo_node_pub.publish(msg_pub);
 }
 
-float vel_ratio(float vel, float lo, float hi)
-{
-    return (vel < lo) ? lo / vel : (vel > hi) ? hi / vel : 1;
-}
--- src/rvo_node.h
+++ src/rvo_node.h
@@ -5,11 +5,11 @@
 #include "geometry_msgs/Twist.h"
 #include "geometry_msgs/Pose.h"
 #include "geometry_msgs/Point.h"
-#include "gazebo_msgs/ModelStates.h"
 #include <string>
 #include "../rvo_lib/nav_rvo.h"
 #include "rvo_ros/SetGoals.h"
-#include "master_msgs/global_info.h"
+#include "rvo_ros/rvo_vel.h"
+#include "gazebo_msgs/WorldState.h"
 
 const int num_max = 10;
 int num_agent = 0;
@@ -17,7 +17,7 @@ int copy_num_agent = 1;
 float vel_ratio(float vel, float lo, float hi);
 // geometry_msgs::Twist *list_obs_twist = new geometry_msgs::Twist();
 ros::Publisher rvo_node_pub;
-gazebo_msgs::ModelStates msg_pub;
+gazebo_msgs::WorldState msg_pub;
 std::vector<geometry_msgs::Point> rvo_goals;
 
 RVO::RVOPlanner* rvo;
